d806b2b06cebd0329f5bc721276b8d4dc5e31d6f
landryliga <ligalandry@gmail.com>
2026-07-21 13:14:41 +0100

    Configuration Vercel pour NestJS

---
+++ backend/api/index.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { AppModule } from '../src/app.module';
+
+const server = express();
+
+export const createServer = async (expressInstance: any) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.enableCors();
+  await app.init();
+};
+
+createServer(server);
+
+export default server;
